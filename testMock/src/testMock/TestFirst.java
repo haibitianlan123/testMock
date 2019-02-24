281cc03615a564d63f0d4ef1925746f929ca7641
haibitianlan123 <534152131@qq.com>
2019-02-24 16:45:53 +0800

    this is my first commit

---
+++ testMock/src/testMock/TestFirst.java
@@ -0,0 +1,10 @@
+package testMock;
+
+public class TestFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a firsrt test");
+	}
+
+}
